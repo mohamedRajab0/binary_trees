e9212fa0d9453d30fea28f07d266cde23dcd8909
mohamedRajab0 <elzaimmohamed@gmail.com>
2024-04-09 12:40:49 +0200

    fix betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,6 +12,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
+	if (!newNode)
+		return (NULL);
 	if (!parent)
 		newNode->parent = parent;
 	newNode->n = value;
