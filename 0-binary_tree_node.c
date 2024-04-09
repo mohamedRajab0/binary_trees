83f274491de0f434f28073d74ffffbe317c6b53d
mohamedRajab0 <elzaimmohamed@gmail.com>
2024-04-09 12:44:07 +0200

    fix betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,6 +15,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (!newNode)
 		return (NULL);
 	newNode->n = value;
+	newNode->parent = parent;
 	newNode->right = NULL;
 	newNode->left = NULL;
 	return (newNode);
