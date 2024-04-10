8d5f20be31a5e63ead8aa6c02bc071bf45523abf
mohamedRajab0 <elzaimmohamed@gmail.com>
2024-04-10 11:07:47 +0200

    upload count of node func

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,7 +7,8 @@
 */
 
 size_t binary_tree_size(const binary_tree_t *tree)
-{   if (!tree)
+{
+	if (!tree)
 		return (0);
 	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
