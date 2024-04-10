04bc9b3df584080bd87e356dcec9966206ee0308
mohamedRajab0 <elzaimmohamed@gmail.com>
2024-04-10 11:49:12 +0200

    upload count of node func

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,10 +1,17 @@
 #include "binary_trees.h"
 
-/***/
+/**
+ * binary_tree_nodes - count of nodes that have at least 1 child
+ * @tree: pointer of the root
+ * Return: node count
+*/
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	if (tree->left || tree->right)
+	if (tree->left != NULL || tree->right != NULL)
 		return (1);
-	return (binary_tree_nodes(tree->right) + binary_tree_nodes(tree->left))
+	else if (tree->left != NULL && tree->right != NULL)
+		return (0);
+
+	return (binary_tree_nodes(tree->right) + binary_tree_nodes(tree->left) );
 }
