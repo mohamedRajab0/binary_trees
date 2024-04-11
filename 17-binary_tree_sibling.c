2ca53aa40478be788270da4bd069453dbcc3e475
mohamedRajab0 <elzaimmohamed@gmail.com>
2024-04-11 09:36:50 +0200

    upload task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_sibling - find sibling of the node
+ * @node: node to find thier sibling
+ * Return: sbling of the node, or NULL if it did not have
+*/
+
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !node->parent)
